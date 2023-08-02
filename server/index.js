4e4dee9dfc80a6ee7284edcdb3a23d2148720a6d
kmanthrige <k.manthrige@gmail.com>
2023-08-01 23:27:20 -0500

    put assignment

---
+++ server/index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const { Pool } = require("pg");
+const bodyParser = require("body-parser")
+
+const PORT = process.env.PORT || 8080
+
+const app = express()
+
+app.use(express.json())
+
+app.use(bodyParser.json());
+
+const pool = new Pool({
+    user: "phitran",
+    host: "localhost",
+    database: "phitran",
+    port: "5432"
+});
+
+app.get("/api/getList", (req, res) => {
+    pool.query("SELECT * FROM chores", (error, result) => {
+        if (error) {
+            console.log("Error Executing Query", error)
+            res.status(500).json({ error: "Internal Server Error" })
+            return
+        }
+        res.json(result.rows)
+    })
+})
+
+app.put("/api/updateList/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { todo, due } = req.body;
+
+        //UPDATE ITEMS IN DB
+        const updateQuery = "UPDATE chores SET todo = $1, due = $2 WHERE id = $3";
+        await pool.query(updateQuery, [todo, due, id]);
+        res.json({ message: "Item updated successfully" })
+
+    } catch (error) {
+        console.log("Error Updating Query", error)
+        res.status(500).json({ error: "Internal Server Error" })
+        return;
+    }
+})
+
+app.post("/api/addNew", (req, res) => {
+    const { todo, due } = req.body
+    console.log("req", req.body)
+    pool.query("INSERT INTO chores (todo, due) VALUES ($1, $2)", [todo, due], (error, result) => {
+        if (error) {
+            console.log("Error executing query", error)
+            res.status(500).json({ message: "Internal Server Error" })
+            return
+        }
+        res.json({ message: "Data Inserted Successfully" })
+    })
+})
+
+app.delete("/api/deleteChore:id", (req, res) => {
+    const id = req.params.id
+    console.log("req.param", req.params.id)
+    pool.query("DELETE FROM chores WHERE id=$1", [id], (error, result) => {
+        if (error) {
+            console.log("Error Executing Query", error)
+            res.status(500).json({ message: "Internal Server Error" })
+            return
+        }
+        res.json({ message: "Data Delete Successfully" })
+    })
+})
+
+// LISTEN TO THE SERVER
+app.listen(PORT, () => {
+    console.log("Server listening on", PORT)
+
+})
--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+export default function App() {
+
+  const [choreList, setChoreList] = useState([])
+  const [newChore, setNewChore] = useState ({
+    todo: "",
+    due: "",
+  })
+
+  const getData = async () => {
+    try {
+      const response = await fetch("/api/getList")
+      const data = await response.json()
+      setChoreList(data)
+    } catch (error) {
+      console.log("Error in GET data", error)
+    }
+  }
+
+  const putData = async () => {
+    const id = 1;
+    const newData = {
+      todo: "Clean room",
+      due: "08/03/2023"
+    }
+    try {
+      const response = await fetch(`/api/updateList/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newData)
+      });
+      const data = await response.json();
+      console.log("new data", data);
+    } catch (error) {
+      console.log("Error in PUT data")
+    }
+  }
+
+  const addData = async () => {
+    try {
+      const response = await fetch("/api/addNew", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newChore)
+      })
+      const data = await response.json()
+      console.log("POST data", data)
+      getData()
+    } catch (error) {
+      console.log("Error in POST data", error)
+    }
+  }
+
+  const deleteData = async (id) => {
+    try{
+      const response = await fetch(`/api/deleteChore${id}`, {
+        method: "DELETE"
+      })
+      const data = await response.json()
+      getData()
+    }catch(error){
+      console.log("Error deleting item", error)
+    }
+  }
+
+  const placeholder = (event) => {
+    setNewChore((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value
+    }))
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  useEffect (() => {
+
+  }, [newChore])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>Adult in Training</div>
+      <div className="placeholder">
+        <input name="todo" className="todo" placeholder="Chore" onChange={placeholder} value={newChore.todo}/>
+        <input name="due" className="due" placeholder="Due Data" onChange={placeholder} value={newChore.due}/>
+      </div>
+      <button onClick={putData}>UPDATE CHORE</button>
+      <button onClick={addData}>ADD NEW CHORE</button>
+      <button onClick={deleteData}>DELETE CHORE</button>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Chore</th>
+            <th>Due Date</th>
+          </tr>
+        </thead>
+        <tbody>
+        {choreList.map((item) => 
+          <tr key={item.id}>
+            <td>{item.todo}</td>
+            <td>{item.due}</td>
+            <td><button onClick={() => deleteData(item.id)}>X</button></td>
+          </tr>
+          )}
+        </tbody>
+      </table>
+    </>
   );
 }
-
-export default App;
